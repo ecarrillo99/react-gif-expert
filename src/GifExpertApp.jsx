b000507ad3411192a616912a3ebb2e0978c6b52f
Edisson <carrilloedisson@gmail.com>
2023-04-10 09:27:53 -0500

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components/";
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch'])
+
+    const onAddCategory=(newCategory)=>{
+
+        if(categories.includes(newCategory)) return;
+        setCategories([newCategory,...categories])
+        //setCategories(cat=>[...cat, 'Valorant'])
+    }
+
+    return (
+        <>
+            {/*titulo*/}
+            <h1>GifExpertApp</h1>
+            {/*Input*/}
+            <AddCategory 
+                //setCategories={setCategories}
+                onNewCategory={(value)=>onAddCategory(value)}
+            />
+            {/*Listado de Gif*/}
+
+            {categories.map(category=>(
+                    <GifGrid 
+                        key={category} 
+                        category={category}/>
+                )
+            )}
+            {/*Gif Item*/}
+        </>
+    )
+}
